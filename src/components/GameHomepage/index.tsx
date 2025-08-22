eef733173e3d1cd41280f0f7b49c91dcaf4ce81c
Shujauddin <msshujauddin@gmail.com>
2025-08-23 04:46:23 +0530

    Restore dev tools in dev branch after main merge - keep dev functionality intact

--- src/components/GameHomepage/index.tsx
+++ src/components/GameHomepage/index.tsx
@@ -11,6 +11,11 @@ const FlappyGame = dynamic(() => import('@/components/FlappyGame'), {
     ssr: false
 });
 
+// Dynamically import DevSignOut only in development
+const DevSignOut = dynamic(() => import('@/components/DevSignOut'), {
+    ssr: false
+});
+
 interface Star {
     x: number;
     y: number;
@@ -258,6 +263,7 @@ export default function GameHomepage() {
                         >
                             Tap To Play
                         </button>
+                        <DevSignOut />
                     </div>
                     <div className="bottom-nav-container">
                         <div className="space-nav-icons">
